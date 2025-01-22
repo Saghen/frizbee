68a9607e4e7ebc9f91ff5bb87a27819f112c4b3e
Liam Dyer <liamcdyer@gmail.com>
2025-01-22 11:23:01 -0500

    feat: filter by max typos, minor perf improvements

--- src/match_.rs
+++ src/match_.rs
@@ -2,8 +2,6 @@
 pub struct Match {
     /** Index of the match in the original list of haystacks */
     pub index_in_haystack: usize,
-    /** Index of the match in the returned list of matches */
-    pub index: usize,
     pub score: u16,
     pub indices: Option<Vec<usize>>,
 }
