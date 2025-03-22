6415483c58742ef66f1fd5e19ee4d122f0eb0534
Liam Dyer <liamcdyer@gmail.com>
2025-03-22 15:45:35 -0400

    feat: increase score bonus for matching case

--- src/const.rs
+++ src/const.rs
@@ -5,7 +5,7 @@ pub const SIMD_WIDTH: usize = 16;
 // NOTE: be vary carefuly when changing these values since they affect what can fit in
 // the u8 scoring without overflowing
 
-pub const MATCH_SCORE: u16 = 7;
+pub const MATCH_SCORE: u16 = 6;
 pub const MISMATCH_PENALTY: u16 = 4; // -4
 pub const GAP_OPEN_PENALTY: u16 = 3; // -3
 pub const GAP_EXTEND_PENALTY: u16 = 1; // -1
@@ -18,7 +18,7 @@ pub const DELIMITER_BONUS: u16 = 4;
 // FIXME: temporarily disabled until we can apply only when the char before the capital is
 // lowercase
 pub const CAPITALIZATION_BONUS: u16 = 0;
-pub const MATCHING_CASE_BONUS: u16 = 1;
+pub const MATCHING_CASE_BONUS: u16 = 2;
 // bonus for haystack == needle
 pub const EXACT_MATCH_BONUS: u16 = 4;
 
