616d2d3dce41e4aea6d473ba7f0bb9c197db1b28
Liam Dyer <liamcdyer@gmail.com>
2025-04-20 21:11:49 -0400

    feat: decrease gap penalties

--- src/const.rs
+++ src/const.rs
@@ -1,7 +1,7 @@
 pub const MATCH_SCORE: u16 = 12; // Score for a match
 pub const MISMATCH_PENALTY: u16 = 8; // Penalty for a mismatch (substitution)
-pub const GAP_OPEN_PENALTY: u16 = 6; // Penalty for opening a gap (deletion/insertion)
-pub const GAP_EXTEND_PENALTY: u16 = 2; // Penalty for extending a gap (deletion/insertion)
+pub const GAP_OPEN_PENALTY: u16 = 3; // Penalty for opening a gap (deletion/insertion)
+pub const GAP_EXTEND_PENALTY: u16 = 1; // Penalty for extending a gap (deletion/insertion)
 
 pub const PREFIX_BONUS: u16 = 12; // Bonus for matching the first character of the haystack
 pub const DELIMITER_BONUS: u16 = 8; // Bonus for matching _after_ a delimiter character (e.g. "hw" on "hello_world", will give a bonus on "w")
