ca69df56b84d3f96df0d3cb348af4069a4997bc1
Liam Dyer <liamcdyer@gmail.com>
2025-08-02 17:10:45 -0400

    docs: fix out of date match_* docs

--- src/one_shot/matcher.rs
+++ src/one_shot/matcher.rs
@@ -6,10 +6,10 @@ use crate::prefilter::string_to_bitmask;
 use crate::smith_waterman::greedy::match_greedy;
 use crate::{Config, Match};
 
-/// Computes the Smith-Waterman score with affine gaps for the list of given targets.
+/// Computes the Smith-Waterman score with affine gaps for a needle against a list of haystacks.
 ///
-/// You should call this function with as many targets as you have available as it will
-/// automatically chunk the targets based on string length to avoid unnecessary computation
+/// You should call this function with as many haystacks as you have available as it will
+/// automatically chunk the haystacks based on string length to avoid unnecessary computation
 /// due to SIMD
 pub fn match_list<S1: AsRef<str>, S2: AsRef<str>>(
     needle: S1,
